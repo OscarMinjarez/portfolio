a37cc80ce2bcf6d3db35cfe58609196e9b600905
OscarMinjarez <minjarez.dev@gmail.com>
2026-03-30 22:30:16 -0700

    feat: add Coolify deployment workflow and disable local dev server configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,13 +30,13 @@ export default defineConfig({
             '@composables': fileURLToPath(new URL('./resources/js/composables', import.meta.url)),
         },
     },
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        cors: true,
-        hmr: {
-            host: '192.168.100.9',
-        }
-    },
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     strictPort: true,
+    //     cors: true,
+    //     hmr: {
+    //         host: '192.168.100.9',
+    //     }
+    // },
 });
